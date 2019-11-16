ae453d6ef2367a2ba90699f2fb6392f2f336e396
yanxlg <xlyan@vova.com.hk>
2019-11-16 11:06:49 +0800

    update version

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ const shelljs = require('shelljs');
 const pkg = require('./package.json');
 const fs = require("fs");
 const path = require("path");
+const chalk = require("chalk");
 
 const argv = require('minimist')(process.argv.slice(2));
 
 gulp.task(
     'publish',
     gulp.series(done => {
-        console.log('publishing');
+        console.log(chalk.greenBright('publishing'));
         const { version } = pkg;
         const readMeFile = path.join(__dirname,"README.md");
         fs.writeFileSync(readMeFile,fs.readFileSync(readMeFile,"utf-8").replace(/(npm-v)[\d\.]+(-blue)/,`$1${version}$2`));
@@ -25,9 +26,9 @@ gulp.task(
         if (pkg.scripts['pre-publish']) {
             shelljs.exec(`npm run pre-publish`);
         }
-        console.log(args.join(' '));
+        console.log(chalk.blue(args.join(' ')));
         let ret = shelljs.exec(args.join(' ')).code;
-        console.log('published');
+        console.log(chalk.greenBright('published'));
         if (!ret) {
             ret = undefined;
         }
@@ -43,11 +44,11 @@ gulp.task(
         shelljs.exec(`git add -A`);
         shelljs.exec(`git commit -m "update version"`);
         shelljs.exec('git push origin master:master');
-        console.log("update version");
+        console.log(chalk.greenBright("update version"));
         shelljs.exec(`git tag ${version}`);
         shelljs.exec(`git push origin ${version}:${version}`);
         shelljs.exec('git push origin master:master');
-        console.log('tagged');
+        console.log(chalk.greenBright('tagged'));
         done();
     })
 );
